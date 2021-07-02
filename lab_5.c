faf728b01805b35082948dfc9153cfe19f7eba1f
josaph <say.dashiev@gmail.com>
2021-07-02 15:26:25 +0800

    English comments

--- lab_5.c
+++ lab_5.c
@@ -8,6 +8,7 @@
 
 void ShowArray(int A[], int n);
 void newWay(int in[], int out[], int n);
+void ShiftR(char S[], int pos, int n);
 void ShiftL(int S[], int pos, int n);
 
 
@@ -15,13 +16,13 @@ int main()
 {
 	setlocale(LC_ALL, "");
 	//int Arr[N] = { 30, 11,25,1,4,0,8,3,2, 5 };
-	int Arr[N] = { 30, 11,25,1,6,26,20,5,7, 3 };
+	int Arr[N] = { 30,11,25,1,6,26,20,5,7,3 };
 	//int Arr[N] = { 1, 2,3,4,5,6,7,8,9,10 };
 	int Arr2[N];
-	printf("Входной массив Arr: \n");
+	printf("First array Arr: \n");
 	ShowArray(Arr, N);
 	newWay(Arr, Arr2,  N);
-	printf("Выходной массив Arr2: \n");
+	printf("Second array Arr2: \n");
 	ShowArray(Arr2, N);
 	
 	return 0;
@@ -40,17 +41,26 @@ void ShowArray(int A[], int n)
 void newWay(int in[], int out[], int n)
 {
 	out[0] = INT_MIN; //max sequence
-	for (int i = 1; i <= n; ++i) /*<=n , +1 element*/
+	for (int i = 1; i <n+1; i++) /*<=n , +1 element*/
 		out[i] = INT_MAX; /*follow array, element which finished max sequence*/
 
 	for (int i = 0; i < n; i++)
-		for (int j = 1; j <= n; j++)
+		for (int j = 1; j < n+1; j++) /*running in out[] array*/
 			if (out[j - 1] < in[i] && in[i] < out[j])
 				out[j] = in[i];
 
 	ShiftL(out, 1, 1); /*act to delete INT_MIN*/
  }	
 
+void ShiftR(char S[], int pos, int n)
+{
+	int i = N-1;
+
+	for (; i >= pos; i--)
+	{
+		S[i + n] = S[i];
+	}
+}
 void ShiftL(int S[], int pos, int n)
 {
 	int i;
