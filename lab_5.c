c2a91ecb10f56db8bfbe4f2a75c7c7f2826eac23
josaph <say.dashiev@gmail.com>
2021-07-28 17:25:33 +0800

    Программа по поиску наиб возр последовательности

--- lab_5.c
+++ lab_5.c
@@ -8,7 +8,6 @@
 #define N 8
 
 void ShowArray(int A[], int n);
-int	F7(int in[], int n);
 int habrLength(int in[], int n);
 int ceilIndex(int subsequence[], int startLeft, int startRight, int key);
 
@@ -36,26 +35,6 @@ void ShowArray(int A[], int n)
 	printf("\n");
 }
 
-int	F7(int in[], int n)
-{
-	int i, j, m, s, t;
-	i = 0, t=0;
-	int digit = in[i];
-	for (s = 1; i < n - 1; i++)
-	{
-		for (j = i + 1, m = 1; j < n; j++)
-			if (digit < in[j])
-				{
-				t = j;
-				digit = in[j];
-				m++;
-				}
-		if (m > s)
-			s = m;
-	}
-	return s;
-}
-
 int habrLength(int in[], int n) 
 {
     /*Если элемент больше максимального элемента в массиве, добавляем элемент в конец, 
@@ -63,77 +42,81 @@ int habrLength(int in[], int n)
     if (n <= 1) 
         return 1;
 
-    int lis_length = 1; /*было -1 , но минимальная длина полседовательности 1*/
+    int lis_length = 1; /*было -1 , но минимальная длина полседовательности 1 (не строки< т.к. в строке все эл-ы идут подряд)*/
 
-    int minElementForLength[N];
-    int indexes[N];
+    int minElementForLength[N]; /*здесь мы храним для каждой длины подпоследовательности минимальный элемент, на которым она может оканчиваться*/
+    int indexes[N]; /*массив в котором хранятся длины последовательностей, которые оканчиваются на этот элемент*/
 
     for (int i = 0; i < n; ++i) 
-        minElementForLength[i] = INT_MAX;
+        minElementForLength[i] = INT_MAX; /*заполняем массив последовательностей маскимальными элеиентами*/
 
     minElementForLength[0] = in[0];
-    indexes[0] = 1; //было 0, длина минимальной послед 1
+    indexes[0] = 0; //было 0, длина минимальной послед 1
 
-    for (int i = 1; i < n; ++i) 
+    for (int i = 1; i < n; i++) /*заполяем массив длин послендовательнностей*/
     {
-        indexes[i] = ceilIndex(minElementForLength, 0, i, in[i]);
+        indexes[i] = ceilIndex(minElementForLength, 0, i, in[i]); /*заполняем параллельно массив indexes, соответственено i*/
 
-        if (lis_length < indexes[i]) 
-            lis_length = indexes[i];
+          if (lis_length < indexes[i]) {
+            lis_length = indexes[i]; /*длина самой длинной подпоследовательности -1, впоследствии значеие индекса*/
+        }
     }
     printf("Массив indexes равен: \n");
     ShowArray(indexes, N);  
     printf("Массив  minElementForLength равен: \n");
     ShowArray(minElementForLength, N);
 
-       return lis_length;
+     return lis_length+1; /*+1 чтобы найти максимальную длину*/
 }
 
-int ceilIndex(int minElementForLength[], int startLeft, int startRight, int key) 
-/*ф-я которая возвращает максимальное кол-во эл-в, котрые могу стоят перед этим числом по возрастанию*/
+int ceilIndex(int minElementForLength[], int startLeft, int startRight, int key)  //(.., 0, i, 5key = in[i])
+/*ф-я которая возвращает максимальное кол-во эл-в, котрые могу стоять перед этим числом по возрастанию*/
 {
 
-    int mid = 0;
-    int left = startLeft; /*индекс мин эл-а массива*/
-    int right = startRight; /*индекс максимального элемента масива*/
-    int ceilIndex = 0;
-    bool ceilIndexFound = false;
+    int mid = 0; /*обнуляем значение середины отрезка, т.к. мы будем работать с новыми данными*/
+    int left = startLeft; /*индекс  эл-а массива с которого начинается отрезок, всегда 0*/
+    int right = startRight; /*индекс элемента на котором заканчивается отрезок*/
+    int ceilIndex = 0; /*нужен для того чтобы присвоить значение в массив длин последовательностей*/
+    bool ceilIndexFound = false; /*флаг*/
+    ShowArray(minElementForLength, N);
 
     for (mid = (left + right) / 2; left <= right && !ceilIndexFound; mid = (left + right) / 2) 
     {
-        if (minElementForLength[mid] > key) 
+        if (minElementForLength[mid] > key) /*если предпосл эл массива  больше ключа*/
             right = mid - 1;
 
         else if (minElementForLength[mid] == key) 
         {
             ceilIndex = mid;
-            ceilIndexFound = true;
+            ceilIndexFound = true;  /*не попадаем в 107*/
         }
         else if (mid + 1 <= right && minElementForLength[mid + 1] >= key) 
+            /*рассматриваемые элемент подпослед-и (х) меньше чем наибольший элемент в массиве (Nmax), но больше чем посл подстроки*/
         {
-            minElementForLength[mid + 1] = key;
-            ceilIndex = mid + 1;
-            ceilIndexFound = true;
+            minElementForLength[mid + 1] = key; /*ставим key в конец, т.е. это число заменяем концом подпослед-и */
+            ceilIndex = mid + 1; /*придаем индекс который равен длине подпосл -1*/
+            ceilIndexFound = true; /*чтобы выйти из цикла for, при true из массива мы не выйдем*/
+            /*не попадаем в 107*/
         }
-        else 
+        else /*вроде бы если ключ больше чем последний эл-т подпоследовательности*/
         {
-            left = mid + 1;
+            left = mid + 1; /*7<8*/
         }
     }
 
-    if (!ceilIndexFound) 
+    if (!ceilIndexFound)  /*если ceilndex found = false, то при инверитовании   !ceilIndexFound = true и входим в тело*/
     {
         if (mid == left)
         {
-            minElementForLength[mid] = key;
+            minElementForLength[mid] = key; /*когда ключ имеет длину 1 эл-т*/
             ceilIndex = mid;
         }
         else 
         {
             minElementForLength[mid + 1] = key;
-            ceilIndex = mid + 1;
+            ceilIndex = mid + 1; /*замена послед эл-а на который оканчивается подпосл, 24 меняем на 8*/
         }
     }
 
-    return ceilIndex+1; /*было без +1, но если какой-то эл-т больше предыдущего то прибавляем+1 */
+    return ceilIndex; /*было без +1, но если какой-то эл-т больше предыдущего то прибавляем+1 */
 }
